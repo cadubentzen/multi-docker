8cd5497b709b5328cb6678326e17b7cf99a44c4f
Carlos Bentzen <carlos.bentzen@teamviewer.com>
2021-11-12 17:31:07 +0100

    Change version name to k8s

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Fib calculator version 3
+            Fib calculator version k8s!!1
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
